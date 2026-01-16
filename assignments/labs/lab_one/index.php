ce3074aa23b1ea9f947796471d790882afa9dece
sebastianrodrigo903 <sebastianrodrigo903@gmail.com>
2026-01-15 21:15:43 -0500

    Update index.php

--- assignments/labs/lab_one/index.php
+++ assignments/labs/lab_one/index.php
@@ -1,4 +1,23 @@
-<?php 
-require "header.php"; 
-echo "<p> Follow the instructions outlined in instructions.txt to complete this lab. Good luck & have fun!😀 </p>";
-require "footer.php"; 
+<?php
+// Load the Car class so it can be used in this file
+include 'car.php';
+
+// Create a Car object
+$car1 = new Car();
+
+// Set values for the car
+$car1->make = "Ford";
+$car1->model = "Focus";
+$car1->year = 2020;
+
+// Display the car details in the browser
+echo $car1->showCarDetails();
+?>  
+
+/*
+Reflection:
+The easiest part of this lab was creating the class and assigning values to the
+object properties. I found using include to connect files understandable after
+practicing it. The more difficult part was making sure everything was organized
+correctly and that the files were linked properly.
+*/
